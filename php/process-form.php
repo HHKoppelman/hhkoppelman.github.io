d20ac5a2ad791f56e455a698211ba5dfb6a832cf
Helmer Koppelman <koppelman@mbp162.wireless.ias.edu>
2020-09-27 16:26:30 -0400

    updating the about page

--- php/process-form.php
+++ php/process-form.php
@@ -1,13 +1,13 @@
 <?php
 if (isset($_REQUEST['name'],$_REQUEST['email'])) {
-      
+
     $name = $_REQUEST['name'];
     $mail = $_REQUEST['email'];
     $subject = $_REQUEST['subject'];
     $message = $_REQUEST['message'];
 
     // Set your email address where you want to receive emails.
-    $to = 'yourmail@mail.com';
+    $to = 'koppelman@ias.edu';
 
     $headers = "From: ".$name." <".$mail."> \r\n";
 
